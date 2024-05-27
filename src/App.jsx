0ac73b07d9315dd2631d037e93764583a2fe1878
userwijnja <56116163+userwijnja@users.noreply.github.com>
2024-05-27 16:57:01 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import React from 'react';
+import ContentFetcher from './ContentFetcher';
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1>My React App</h1>
+      <ContentFetcher />
+    </div>
+  );
+};
+
+export default App;
